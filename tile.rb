8b558958872215fbeafa76f35e7b8e1a38daf832
jinfull <jinfull@gmail.com>
2019-02-19 15:34:09 -0800

    added #uncover method to Tile class

--- tile.rb
+++ tile.rb
@@ -4,10 +4,15 @@ class Tile
     attr_reader :is_bomb
     attr_accessor :adj_mine_count, :face_up
 
-    def initialize(is_bomb, adj_mine_count = nil)
+    def initialize(is_bomb)
         @is_bomb = is_bomb
-        @adj_mine_count = adj_mine_count
+        @adj_mine_count = nil
 
         @face_up = false
+    end
+
+    def uncover
+        @face_up = true
+        nil
     end
 end
